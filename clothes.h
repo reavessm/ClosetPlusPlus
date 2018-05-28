9dc422f9413c172399a8a00cfe125ada821694fc
Stephen Reaves <reavessm@email.sc.edu>
2018-05-28 09:49:43 -0400

    :constuction: Added all globals to interface.

--- clothes.h
+++ clothes.h
@@ -1,9 +1,10 @@
 /******************************************************************************
- * File Name: shirt.h
+ * File Name: clothes.h
  * Created by: Stephen M. Reaves
  * Date Last Modified:
  *
- * Summary: Header File for 'Shirt' class in the Closet++ program
+ * Summary: Clothes abstract class to be used by members of closet.
+ *          Handles the light work.
  *
  */
 
@@ -42,25 +43,45 @@ class Clothes {
     string GetPrimColor() { return primary_color_; }
     string GetSecColor() { return secondary_color_; }
     string GetTertColor() { return tertiary_color_; }
-    string GetPattern() {return pattern_; }
+    string GetPattern() { return pattern_; }
+    string GetMaterial() { return material_; }
+    string GetStyle() { return style_; }
+    string GetLength() { return length_; }
+    string GetCut() { return cut_; }
+    string GetSleeveLength() { return sleeve_length_; }
+    string GetCollar() { return collar_; }
 
     void SetName(string name) { name_ = name; }
     void SetPrimColor(string color) { primary_color_ = color; }
     void SetSecColor(string color) { secondary_color_ = color; }
     void SetTertColor(string color) { tertiary_color_ = color; }
     void SetPattern(string pattern) { pattern_ = pattern; }
+    void SetMatterial(string material) { material_ = material; }
+    void SetStyle(string style) { style_ = style; }
+    void SetLength(string length) { length_ = length; }
+    void SetCut(string cut) { cut_ = cut; }
+    void SetSleeveLength(string length) { sleeve_length_ = length; }
+    void SetCollar(string collar) { collar_ = collar; }
 
-    //bool IsDirty();
-
+ // These are what makes this a purely abstract class.  The implementation of 
+ // these will be changed from one article of clothing to the next.
+    // virtual bool IsDirty() = 0;
     virtual string ToString() const = 0;
 
   protected:
+    // Set Globals
+    int id_ = kDummyID;
     string name_ = kDummyName;
     string primary_color_ = kDummyPrimColor;
     string secondary_color_ = kDummySecColor;
     string tertiary_color_ = kDummyTertColor;
     string pattern_ = kDummyPattern;
-    int id_ = kDummyID;
+    string material_ = kDummyMaterial;
+    string style_ = kDummyStyle;
+    string length_ = kDummyLength;
+    string cut_ = kDummyCut;
+    string sleeve_length_ = kDummySleeveLength;
+    string collar_ = kDummyCollar;
 
 };
 
--- shirt.h
+++ shirt.h
@@ -15,17 +15,21 @@
 #include <iostream>
 #include <string>
 
+#include "clothes.h"
+
 using namespace std;
 
 
-/* const int kDummyShirtID = -1;
+/* Implemented with Interface
+const int kDummyShirtID = -1;
 const string kDummyShirtName = "Fake Name";
 const string kDummyShirtPrimColor = "Fake Primary Color";
 const string kDummyShirtSecColor = "Fake Secondary Color";
 const string kDummyShirtTertColor = "Fake Tertiary Color";
 const string kDummyShirtPattern = "Fake Pattern";
 const string kDummyShirtSleeveLength = "Fake Sleeve Length";
-const string kDummyShirtCollar = "Fake Collar"; */
+const string kDummyShirtCollar = "Fake Collar"; 
+*/
 
 class Shirt {
   public:
@@ -37,22 +41,26 @@ class Shirt {
     virtual ~Shirt();
 
     //General Functions
+/* Implemented with Interface
     int GetID();
     string GetName();
     string GetPrimColor();
     string GetSecColor();
     string GetTertColor();
     string GetPattern();
-    string GetSleeveLength();
-    string GetCollar();
+*/
+    string GetSleeveLength() { return sleeve_length_; }
+    string GetCollar() { return collar_; }
 
+/* Implemented with Interface
     void SetName(string name);
     void SetPrimColor(string color);
     void SetSecColor(string color);
     void SetTertColor(string color);
     void SetPattern(string pattern);
-    void SetSleeveLength(string length);
-    void SetCollar(string collar);
+*/
+    void SetSleeveLength(string length) { sleeve_length_ = length; }
+    void SetCollar(string collar) { collar_ = collar; }
 
     //bool IsDirty();
 
@@ -61,14 +69,16 @@ class Shirt {
   
   private:
     //Variables
+/* Implemented with Interface
     int id_ = kDummyShirtID;
     string name_ = kDummyShirtName;
     string primary_color_ = kDummyShirtPrimColor;
     string secondary_color_ = kDummyShirtSecColor;
     string tertiary_color_ = kDummyShirtTertColor;
     string pattern_ = kDummyShirtPattern;
-    string sleeve_length_ = kDummyShirtSleeveLength;
-    string collar_ = kDummyShirtCollar;
+*/
+    string sleeve_length_ = kDummySleeveLength;
+    string collar_ = kDummyCollar;
 
     //Private Functions
 
