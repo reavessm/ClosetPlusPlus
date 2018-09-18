309ba82be2ad08a00e8a61d72db0da8d342a7e3d
Stephen Reaves <reavessm@email.sc.edu>
2018-09-17 21:46:16 -0400

    :construction: :rocket: Started adding SQLite3 support

---
+++ sql.h
@@ -0,0 +1,63 @@
+/**
+ * @class SQL
+ * @author Stephen M. Reaves
+ * @headerfile sql.h
+ * @date Sep 18, 2018
+ */
+
+#include <cstdlib>
+#include <cstring>
+#include <stdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sqlite3.h>
+#include "backend.h"
+
+using namespace std;
+
+#ifndef SQL_H
+#define SQL_H
+
+class SQL : public Backend {
+ public:
+  // Constructors and Destructors
+  SQL();
+  virtual ~SQL();
+  bool Init();
+  void Die();
+  bool Store(string file = kDummyFileName) const { return true; };
+  bool Open(string file = kDummyFileName) const { return true; };
+  void MakeCloset(){};
+  string GetClosetName();
+  string ToString() const;
+
+  // Inserts for each type of clothing
+  // @todo Refactor?
+  bool InsertShirt(string name, string primary_color, string secondary_color,
+                   string tertiary_color, string pattern, string sleeve_length,
+                   string collar);
+  bool InsertPants(string name, string primary_color, string secondary_color,
+                   string tertiary_color, string material, string length,
+                   string cut);
+  bool InsertSocks(string name, string primary_color, string secondary_color,
+                   string tertiary_color, string pattern);
+  bool InsertShoes(string name, string primary_color, string secondary_color,
+                   string tertiary_color, string material, string style);
+  bool InsertBelt(string name, string primary_color, string secondary_color,
+                  string tertiary_color, string material, string pattern);
+
+ private:
+  // Variables
+  string closet_name_ = kDummyClosetName;
+  sqlite3 *db; ///< DB connection
+  char *sql; ///< Prepared SQL statements
+  char *zErrMsg = 0;
+  int rc;
+
+  // Functions
+  int AssignID(string type);
+  static int callback(void *NotUsed, int argc, char **argv, char **azColName);
+};
+
+#endif /* SQL_H */
